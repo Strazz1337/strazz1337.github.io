6f41c73aa51e69c2f580b7678b76c54c0d45fb85
Kevin <itskevinb@gmail.com>
2020-06-21 18:03:30 +0200

    joe

--- site.js
+++ site.js
@@ -1,8 +1,11 @@
 let nav = document.getElementById("js_navitems");
 let navToggle = document.getElementById("js_navbar_toggle");
+let navIcon = document.getElementById("navbar_icon");
 
 navToggle.addEventListener('click', function () {
     nav.classList.toggle('active');
+    navIcon.classList.toggle('fa-bars');
+    navIcon.classList.toggle('fa-times-circle');
 });
 
 function removeDropdownMenu() {
