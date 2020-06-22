94b3f380c7f7b687d3a6bb088c225f27f8231cf0
Kevin <itskevinb@gmail.com>
2020-06-22 14:20:41 +0200

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,7 @@
+$(document).ready(function () {
+    getMenus();
+});
+
 let nav = document.getElementById("js_navitems");
 let navToggle = document.getElementById("js_navbar_toggle");
 let navIcon = document.getElementById("navbar_icon");
@@ -14,4 +18,34 @@ function removeDropdownMenu() {
         navIcon.classList.toggle('fa-bars');
         navIcon.classList.toggle('fa-times-circle');
     }
+}
+
+function getMenus() {
+    return new Promise(
+        function (resolve, reject) {
+            $.ajax(
+                {
+                    url: "/api/menu",
+                    type: "GET",
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+
+                    success: function (data) {
+                        for (i = 0; i < data.length; i++) {
+                            createMenuButtons(data, i);
+                        }
+                    },
+                    error: function (error) {
+                        popup.show(false, error);;
+                    },
+
+                });
+        });
+}
+
+function createMenuButtons(data, menuid) {
+    let menubutton = document.createElement("button");
+    menubutton.innerHTML = data[menuid].name;
+    menubutton.classList.add("buttonpill");
+    document.getElementById("menu_list").appendChild(menubutton);
 }
